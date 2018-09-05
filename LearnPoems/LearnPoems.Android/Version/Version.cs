8f70b867566b61bd3b4155907096658b9d58ca09
AlanD <alandix@yahoo.com>
2018-09-05 03:03:21 +0100

    Added help page content and versioning

---
+++ LearnPoems/LearnPoems.Android/Version/Version.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using LearnPoems.Version;
+
+// https://xamarinhelp.com/xamarin-forms-dependency-injection/
+[assembly: Xamarin.Forms.Dependency(typeof(LearnPoems.Droid.Version.Version))]
+namespace LearnPoems.Droid.Version
+{
+    public class Version : IVersion
+    {
+        string IVersion.VersionName
+        {
+            get
+            {
+                return Application.Context.ApplicationContext.PackageManager.GetPackageInfo(Application.Context.ApplicationContext.PackageName, 0).VersionName;
+            }
+        }
+    }
+}
