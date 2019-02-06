d410a470fb0af2a915f34e891c48b1f465a08a05
AlanD <riemannzetazero@gmail.com>
2019-02-06 19:09:33 +0000

    Syncfusion licencing

--- LearnPoems/LearnPoems/App.xaml.cs
+++ LearnPoems/LearnPoems/App.xaml.cs
@@ -4,23 +4,19 @@ using System.IO;
 using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace LearnPoems
 {
     public partial class App : Application
 	{
         public static string AppName = "LearnPoems";
-
         public static Settings.Settings Settings { get; set; }
         public static Model.Model Model { get; set; }
         public static FileRepository FileRepository { get; set; }
-
         public static string SystemFolderPath { get; set; }
         public static string PoemFolderPath { get; set; }
         public static string SettingsFileName = "Settings";
         public static string PoemFolderName = "Poems";
-
         // Pages
         public static StartPage StartPage { get; set; }
         public static ViewPoemPage ViewPoemPage { get; set; }
@@ -28,20 +24,15 @@ namespace LearnPoems
         public static HelpPage HelpPage { get; set; }
         public static RecentPage RecentPage { get; set; }
         public static SavePoemPage SavePoemPage { get; set; }
-
         public static PoemViewer PoemViewer { get; set; }
-
         public static readonly double GoldenRatio = 1.6180339887;
-
         static App()
         {
             Model = new Model.Model();
-
             //Register Syncfusion license
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(Model.SyncFusionLicenseKey);
-
+            // Based on https://www.syncfusion.com/kb/9179/syncfusion-license-register-without-hardcoded
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("=====##SyncfusionLicense##=====");
             Settings = new Settings.Settings();
-
             StartPage = new StartPage();
             ViewPoemPage = new ViewPoemPage();
             ChoosePoemPage = new ChoosePoemPage();
@@ -49,23 +40,19 @@ namespace LearnPoems
             RecentPage = new RecentPage();
             SavePoemPage = new SavePoemPage();
         }
-
         public App()
         {
             InitializeComponent();
             MainPage = new NavigationPage(StartPage);
         }
-
         protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
-
 		protected override void OnSleep ()
 		{
 			// Handle when your app sleeps
 		}
-
 		protected override void OnResume ()
 		{
 			// Handle when your app resumes
