8440e7fc5d73e6775b5e1c05414ca6964e23365f
AlanD <alandixon@users.noreply.github.com>
2022-01-01 23:22:15 +0000

    Added Help content

--- LearnPoems/LearnPoems/Model/Model.cs
+++ LearnPoems/LearnPoems/Model/Model.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using LearnPoems.Text;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace LearnPoems.Model
 {
-    public class Model
+    public class Model : INotifyPropertyChanged
     {
+        public Model()
+        {
+
+        }
+
+        //private string copyrightText;
+        public string CopyrightText
+        {
+            get { return HelpPageText.Copyright; }
+            //set { copyrightText = value; NotifyPropertyChanged("CopyrightText"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
     }
 }
---
+++ LearnPoems/LearnPoems/Text/HelpPageText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnPoems.Text
+{
+    public class HelpPageText
+    {
+        public static readonly string Copyright =
+            "Make sure that you aren't breaking copyright when adding content to " + App.AppName;
+    }
+}
