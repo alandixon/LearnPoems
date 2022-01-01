8b90e78bb7546eeeae71766d0dddf6b5629ff1ad
AlanD <alandixon@users.noreply.github.com>
2022-01-01 23:22:48 +0000

    Added Unit test project, but it doesn't work: MS Test and Xamarin looks like a 'mare ...

---
+++ Test_LearnPoems/Test_Helpers.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xamarin.Forms;
+
+namespace Test_LearnPoems
+{
+    [TestClass]
+    public class Test_Helpers
+    {
+        [TestInitialize]
+        public void testInit()
+        {
+
+        }
+
+        [DataRow("a", 1, "a")]
+        [DataRow("ba", 1, "b")]
+        [DataTestMethod]
+        public void Test_RestrictStringLength(string inStr, int len, string expectedOutStr)
+        {
+            string foundOutStr = LearnPoems.Helpers.StringHelper.RestrictStringLength(inStr, len);
+            Assert.AreEqual(expectedOutStr, foundOutStr);
+        }
+    }
+}
