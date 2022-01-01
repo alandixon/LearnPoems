b523456ee3a35788e33eebfd924849b5e4804277
AlanD <alandixon@users.noreply.github.com>
2022-01-01 23:22:39 +0000

    Created ExtendedLabel to manage Tapping ... Added some help when starting to view a poem Initial state management of poem display

--- LearnPoems/LearnPoems/Pages/ChoosePoemPage.xaml.cs
+++ LearnPoems/LearnPoems/Pages/ChoosePoemPage.xaml.cs
@@ -36,7 +36,7 @@ namespace LearnPoems.Pages
         private async void PoemListView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             App.FileRepository.SelectedPoem = e.Item as Poem;
-            App.PoemViewer.AddPoem(App.FileRepository.SelectedPoem);
+            App.PoemViewer.StartLoadPoem(App.FileRepository.SelectedPoem);
             Log.Debug(logTag, "PoemListView_ItemTapped() on poem " + App.FileRepository.SelectedPoem.Name);
             await Navigation.PushAsync(App.ViewPoemPage);
         }
---
+++ LearnPoems/LearnPoems/Text/PoemViewerHelpText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearnPoems.Text
+{
+    public class PoemViewerHelpText
+    {
+        public static readonly string NextLineTap_Help =
+            "Tap the first blank line (here) to show the next line";
+
+        public static readonly string TitleTap_Help =
+            "Tap the title above to show the whole poem";
+
+        public static readonly string PreviousLineTap_Help =
+            "Tap a displayed poem line to remove it and work backwards";
+
+    }
+}
